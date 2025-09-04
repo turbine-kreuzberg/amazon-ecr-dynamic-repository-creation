c8b83a4ff9d4bbed76d7973d59ccd5d34ebbd54c
David Sauer <david.sauer@turbinekreuzberg.com>
2025-09-04 16:31:07 +0200

    use a much shorter full_name

--- ecr/locals.tf
+++ ecr/locals.tf
@@ -2,7 +2,7 @@ locals {
   common_tags        = merge({ "managed-by" = "terraform" })
   current_region     = data.aws_region.current.name
   current_account_id = data.aws_caller_identity.current.account_id
-  full_name          = "dynamic-create-ecr-repo-${local.current_region}${local.name_suffix}"
+  full_name          = "ecr-create-on-push-${local.name_suffix}"
   repo_tags          = merge(var.REPO_TAGS)
   name_suffix        = var.name != "" ? "-${var.name}" : ""
 }
